b84b1e21d8a3f0d78c4843656dc55f75737a9aad
parasite <premkmar395@gmail.com>
2025-10-16 14:48:12 +0530

    Thread-sleep

---
+++ Sleeps.java
@@ -0,0 +1,19 @@
+public class Sleeps extends Thread {
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(getName() + " running...");
+            try {
+                Thread.sleep(900,1000);
+            } catch (InterruptedException e) {
+                System.out.println(getName() + " interrupted!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Sleeps t1 = new Sleeps();
+        Sleeps t2 = new Sleeps();
+        t1.start();
+        t2.start();
+    }
+}
