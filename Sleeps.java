6f8e6c39052675a17327a6762aeb25bf29ac1298
parasite <premkmar395@gmail.com>
2025-11-10 12:55:53 +0530

    Thread concepts

--- Sleeps.java
+++ Sleeps.java
@@ -1,7 +1,7 @@
 public class Sleeps extends Thread {
     public void run() {
         for (int i = 1; i <= 3; i++) {
-            System.out.println(getName() + " running...");
+            System.out.println(Thread.currentThread().getName() + " running...");
             try {
                 Thread.sleep(900,1000);
             } catch (InterruptedException e) {
